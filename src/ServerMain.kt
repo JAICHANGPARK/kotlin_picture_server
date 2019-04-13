deb645cafd2e28a47b77a4524f573388141c9f31
Dreamwalker <aristojeff@gmail.com>
2019-04-13 20:52:45 +0900

    Update ServerMain.kt

--- src/ServerMain.kt
+++ src/ServerMain.kt
@@ -2,12 +2,28 @@ import com.sun.security.ntlm.Server
 import java.net.ServerSocket
 import java.net.Socket
 
+const val TIMEOUT = 3000
+const val ROOT = "C:\\PictureData"
+
 fun main(args: Array<String>) {
-    val receivePort = 1255
+
     var socket: Socket?
     var serverSocket : ServerSocket
 
     val serverLayout = ServerLayout()
+
+
+    var path : String?
+    var readDate : String
+    var oldDate : String
+    
+    var date: Long
+    var currentYear: Int
+    var currentMonth: Int
+    var previousYear: Int
+    var previousMonth: Int
+    val receivePort = 1255
+    val connectPort = 1256
 
     serverLayout.setPCName()
     serverLayout.setIP()
